32045e9d15988aba61dfa34f3487a9940c3e8d46
Alex Sandridge <asandridge@spotx.tv>
2020-12-31 18:56:02 -0700

    Add node server

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,6 @@ import { currencyMap } from '../constants'
 
 const NOMICS_KEY = process.env.REACT_APP_NOMICS_KEY
 
-console.log(NOMICS_KEY)
-
 const now = new Date();
 const endDate = now.toISOString().substring(0, 10);
 let startDate = now;
@@ -13,13 +11,21 @@ startDate = startDate.toISOString().substring(0, 10);
 
 export const fetchCoinHistory = async (coinType) => {
 
-    const url = `https://api.nomics.com/v1/exchange-rates/history?key=${NOMICS_KEY}&currency=${currencyMap[coinType]}&start=${startDate}T00%3A00%3A00Z&end=${endDate}T00%3A00%3A00Z`;
-
     try {
         if (localStorage.getItem(currencyMap[coinType])) {
             return JSON.parse(localStorage.getItem(currencyMap[coinType]))
         }
-        const history = await axios.get(url);
+        const history = await fetch('/get_currency_data', {
+            method: "get",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              coinType: currencyMap[coinType]
+            })
+        });
+        console.log(history, typeof(history))
         localStorage.setItem(currencyMap[coinType], JSON.stringify(history.data))
         return history.data;
     } catch (err) {
